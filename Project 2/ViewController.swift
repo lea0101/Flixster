41c695b03abc2271697cce60c830e01354d43112
lea0101 <70765843+lea0101@users.noreply.github.com>
2023-01-31 16:31:55 -0500

    Initial Commit

---
+++ Project 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 2
+//
+//  Created by Leanne A on 1/31/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
